8e52d6df41aa80cff751b257be57064fbe3571a8
aravinth2760 <aravinth2760@gmail.com>
2026-01-08 17:16:44 +0530

    Finish about implementation [08 JAN 2026]

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,7 +1,28 @@
+import Hero from "../components/Hero";
+import Property from "../components/Property";
+import ContactAgent from "../components/Contact-agent";
+import Agent from "./property/Agent";
+
 const About = () => {
   return (
     <>
-      <h1>About Page</h1>
+      <Hero
+        renderContent={() => (
+          <>
+            <h1 className="display-6 fw-bold mb-3">About Us</h1>
+            <div className="d-flex gap-2 text-uppercase text-muted">
+              <p style={{ color: "var(--primary)" }}>Home</p>
+              <span>/</span>
+              <p style={{ color: "var(--primary)" }}>pages</p>
+              <span>/</span>
+              <p>about</p>
+            </div>
+          </>
+        )}
+      />
+      <Property />
+      <ContactAgent />
+      <Agent />
     </>
   );
 };
